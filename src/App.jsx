0219057b010c139c8dac4d820f869db5d358074e
daniel hnd <danylahendousinabad@gmail.com>
2025-03-22 10:30:39 +0300

    this is my first commit for personal portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello daniel hendosinabad </h1>
+    </>
+  )
+}
+
+export default App
